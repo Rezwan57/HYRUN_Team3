bcacce9bfec7ba2395188a809b63cc1a09a361cc
ynanda01 <240281753@aston.ac.uk>
2025-01-31 03:44:40 +0000

    done

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from "next";
 import Image from "next/image";
 import HeroSection from "@/components/Homepage/HeroSection";
+import HotProducts from "@/components/Homepage/HotProducts";  
+import Footer from "@/components/Homepage/Footer";
+
 
 
 export const metadata: Metadata = {
@@ -13,6 +16,9 @@ export default function Home() {
   return (
     <main className="flex flex-col gap-20 mb-10">
       <HeroSection />
+     
+      <HotProducts />
+      <Footer />
     </main>
   );
 }
