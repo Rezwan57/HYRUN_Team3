cd4532c0f4e61537a43db1d58227229eaa8a4918
ynanda01 <240281753@aston.ac.uk>
2025-02-03 22:59:07 +0000

    chnages made in Hot products section

--- src/components/Homepage/Footer.jsx
+++ src/components/Homepage/Footer.jsx
@@ -1,79 +1,143 @@
-import React from 'react';
-import { FaFacebookF, FaTwitter, FaInstagram, FaWhatsapp } from 'react-icons/fa';
-import './Footer.css';
+import React from "react";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaWhatsapp,
+} from "react-icons/fa";
+import "./Footer.css";
+import { FaTiktok } from "react-icons/fa6";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="footer_area">
-      <div className="footer_info">
-        
+    <footer className="footer-container">
+      <div className="footer-content">
         {/* Logo & Contact Info */}
-        <div className="footer_section">
-          <img src="/assets/logo/Logo.png" alt="HYRUN Logo" className="footer_logo" />
-          <p className="footer_description">
-            Discover the ultimate destination for sneaker enthusiasts. Our vibrant 
-            collection offers a curated selection of stylish and high-quality sneakers 
-            for every occasion.
+        <div className="footer-section">
+          <Image
+            src="/assets/logo/Logo.png"
+            alt="HYRUN Logo"
+            className="footer-logo"
+            width={150}
+            height={150}
+          />
+          <p className="footer-description">
+            Discover the ultimate destination for sneaker enthusiasts. Our
+            vibrant collection offers a curated selection of stylish and
+            high-quality sneakers for every occasion.
           </p>
         </div>
 
-        {/* Useful Links */}
-        <div className="footer_section">
-          <h3 className="footer_title">Useful Links</h3>
-          <ul className="footer_list">
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Shop</a></li>
-            <li><a href="#">FAQs</a></li>
-            <li><a href="/contact-us">Contact Us</a></li>
+        {/* About Section */}
+        <div className="footer-section">
+          <h3 className="footer-title">Useful Links</h3>
+          <ul className="footer-list">
+            <li>
+              <Link className="hover:text-[var(--primary)]" href="about-us">About Us</Link>
+            </li>
+            <li>
+              <Link className="hover:text-[var(--primary)]" href="#">Delivery Information</Link>
+            </li>
+            <li>
+              <Link className="hover:text-[var(--primary)]" href="#">Privacy Policy</Link>
+            </li>
+            <li>
+              <Link className="hover:text-[var(--primary)]" href="#">Terms & Conditions</Link>
+            </li>
+            <li>
+              <Link className="hover:text-[var(--primary)]" href="/contact-us">Contact Us</Link>
+            </li>
           </ul>
         </div>
 
         {/* My Account */}
-        <div className="footer_section">
-          <h3 className="footer_title">My Account</h3>
-          <ul className="footer_list">
-            <li><a href="#">Sign In</a></li>
-            <li><a href="#">View Cart</a></li>
-            <li><a href="#">Returns</a></li>
-            <li><a href="#">Order Status</a></li>
-            <li><a href="#">Help</a></li>
+        <div className="footer-section">
+          <h3 className="footer-title">My Account</h3>
+          <ul className="footer-list">
+            <li>
+              <Link className="hover:text-[var(--primary)]" href="/login">Sign In</Link>
+            </li>
+            <li>
+              <Link className="hover:text-[var(--primary)]" href="#">View Cart</Link>
+            </li>
+            <li>
+              <Link className="hover:text-[var(--primary)]" href="#">Returns</Link>
+            </li>
+            <li>
+              <Link className="hover:text-[var(--primary)]" href="#">Order Status</Link>
+            </li>
+            <li>
+              <Link className="hover:text-[var(--primary)]" href="#">Help</Link>
+            </li>
           </ul>
         </div>
 
-        {/* Secured Payment Gateways */}
-        <div className="footer_section">
-          <h3 className="footer_title">Secured Payment Gateways</h3>
-          <div className="footer_payments">
-            <img src="/assets/SM logos/visa.png.jpg" alt="Visa" className="footer_payment" />
-            <img src="/assets/SM logos/mastercard.png" alt="MasterCard" className="footer_payment" />
-            <img src="/assets/SM logos/mastero.png.jpg" alt="Maestro" className="footer_payment" />
+        <div className="footer-section">
+          <h3 className="footer-title">Secured Payment Gateways</h3>
+          <div className="footer-payments">
+            <Image
+              src="/assets/SM logos/visa.svg"
+              width={50}
+              height={50}
+              alt="Visa"
+              className="footer-payment"
+            />
+            <Image
+              src="/assets/SM logos/mastercard.svg"
+              width={50}
+              height={50}
+              alt="MasterCard"
+              className="footer-payment"
+            />
+            <Image
+              src="/assets/SM logos/maestro.svg"
+              width={50}
+              height={50}
+              alt="Maestro"
+              className="footer-payment"
+            />
+            <Image
+              src="/assets/SM logos/amex.svg"
+              width={50}
+              height={50}
+              alt="American Express"
+              className="footer-payment"
+            />
           </div>
         </div>
 
-        {/* Contact */}
-        <div className="footer_section">
-          <h3 className="footer_title">Contact</h3>
-          <ul className="footer_list">
-            <li><span className="footer_bold">Address:</span> Aston University</li>
-            <li><span className="footer_bold">Phone:</span> +0123456789</li>
-            <li><span className="footer_bold">Email:</span> hyrunwebsite@gmail.com</li>
+        <div>
+          <ul className="footer-list">
+            <h3 className="footer-title">Contact</h3>
+            <li>
+              <span className="footer-bold">Address:</span> Aston University
+            </li>
+            <li>
+              <span className="footer-bold">Phone:</span> +0123456789
+            </li>
+            <li>
+              <span className="footer-bold">Email:</span> hyrunwebsite@gmail.com
+            </li>
           </ul>
-          <div 
-            className="footer_icons">
+
+          <div className="footer-icons">
             <FaFacebookF className="footer-icon" />
             <FaTwitter className="footer-icon" />
             <FaInstagram className="footer-icon" />
             <FaWhatsapp className="footer-icon" />
-            
+            <FaTiktok className="footer-icon" />
           </div>
         </div>
-        
       </div>
-      <hr className="footer_line" />
-      <div 
-          className="footer_copyright">© 2025 HYRUN. All Rights Reserved.
+
+      <div className="bg-prime py-2">
+        <p className="footer-copyright">
+          Copyright &copy; 2025 - All Right Reserved.
+        </p>
       </div>
-    </div>
+    </footer>
   );
 };
 
