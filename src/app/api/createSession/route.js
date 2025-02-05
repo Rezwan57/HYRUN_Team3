65da36c42df92a860c4223e6fb91d3bf89d059d1
Rezwan <rezwan57@outlook.com>
2025-02-05 23:13:42 +0000

    Add user signup API and user list component; update database connection

--- src/app/api/createSession/route.js
+++ src/app/api/createSession/route.js
@@ -1,33 +1,12 @@
-import dbConnection from "../../../../lib/database.js";
-import { NextResponse } from "next/server.js";
+import { db } from "../../../../lib/db.js";
 
 
 
-//GET Data
 export async function GET() {
-    try {
-        const db = await dbConnection();
-        const [rows] = await db.execute("SELECT * FROM users");
-        return NextResponse.json(rows);
-    } catch (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 });
-    }
+  try {
+    const [rows] = await db.query('SELECT * FROM users'); 
+    return Response.json(rows);
+  } catch (error) {
+    return Response.json({ error: 'Database error', details: error }, { status: 500 });
+  }
 }
-
-export async function POST(request) {
-    const body = await request.json();
-    const email = body.user_email;
-    const password = body.user_password;
-
-    const connection = await dbConnection();
-
-    const [rows] = await connection.query("SELECT * FROM users WHERE email = ? AND password = ?", [email, password]);
-
-    if (rows.length > 0) {
-        const user = rows[0];
-        return new Response(JSON.stringify(user), { status: 200 });
-    } else {
-        return new Response(null, { status: 401 });
-    }
-}
-
