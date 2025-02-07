bf404bcfa0bc00b081d4090d3ba0b9ea42b7bc31
Ubka12 <ubahkahie@outlook.com>
2025-02-07 10:18:06 +0000

    w

---
+++ src/components/ContactUs/route.js
@@ -0,0 +1,14 @@
+export async function POST(request: NextRequest) {
+    const body = await request.json()
+    const res = await fetch('/api/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    const data = await res.json();
+    return NextResponse.json(data)
+  
+  }
+  
