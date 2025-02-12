cb649373287d1e6efc8aa4ff1833d8a73ca75b60
Rezwan <rezwan57@outlook.com>
2025-02-12 21:37:29 +0000

    add admin dashboard layout, components, and authentication API endpoints

---
+++ src/app/(admin)/components/Sidebar.jsx
@@ -0,0 +1,69 @@
+'use client';
+import Link from "next/link";
+import React, { useState } from "react";
+import { FaChevronRight, FaChevronDown } from "react-icons/fa6";
+import { motion } from "framer-motion";
+
+const adminLinks = [
+  { id: 1, name: "Dashboard", link: "/admin" },
+  { id: 2, name: "Products", link: "/admin/add-product" },
+];
+
+const editLinks = [
+  { id: 1, name: "Homepage Edit", link: "/admin/edit/homepage" },
+  { id: 2, name: "Contact Edit", link: "/admin/edit/contact" },
+  { id: 3, name: "More Important", link: "/admin/edit/important" },
+];
+
+function Sidebar() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  return (
+    <aside className=" bottom-0 p-4 flex flex-col justify-between flex-shrink-0 gap-10 w-1/6 h-screen bg-neutral-100">
+      <h1>Admin Panel</h1>
+
+      <div className="flex flex-col gap-2 h-full w-full">
+        {adminLinks.map((link) => (
+          <Link
+            href={link.link}
+            key={link.id}
+            className="hover:text-yellow-600 rounded-md p-2"
+          >
+            {link.name}
+          </Link>
+        ))}
+
+        <div>
+          <button
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            className="flex justify-between items-center hover:text-yellow-600 rounded-md p-2 w-full text-left"
+          >
+            <p>Edit Page</p> 
+            {isDropdownOpen ? <FaChevronDown /> : <FaChevronRight />}
+          </button>
+          {isDropdownOpen && (
+            <motion.div
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -10 }}
+              transition={{ duration: 0.3 }}
+             className="flex flex-col pl-3">
+              {editLinks.map((link) => (
+                <Link
+                  href={link.link}
+                  key={link.id}
+                  className="hover:text-yellow-600 p-2"
+                >
+                  {link.name}
+                </Link>
+              ))}
+            </motion.div>
+          )}
+        </div>
+
+      </div>
+    </aside>
+  );
+}
+
+export default Sidebar;
--- src/app/(auth)/(admin-auth)/admin-login/page.jsx
+++ src/app/(auth)/(admin-auth)/admin-login/page.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+'use client';
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 import { FaUser } from "react-icons/fa";
 import { FaKey } from "react-icons/fa";
 
 export default function AdminLogin() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!username || !password) {
+      setError("All fields are required!");
+      return;
+    }
+
+    try {
+      const res = await fetch("/api/admin-login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (res.status === 200) {
+        const data = await res.json();
+        console.log(data);
+        router.push("/admin");
+      } else {
+        setError("Invalid credentials!");
+      }
+    } catch (error) {
+      console.error(error);
+      setError("An error occurred. Please try again.");
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen px-2 overflow-hidden bg-neutral-950 gap-10">
       <div className="text-center flex flex-col gap-2">
@@ -10,12 +45,12 @@ export default function AdminLogin() {
         <h3 className="text-white">Login to your Admin Account</h3>
       </div>
 
-      <form action="submit" className="flex flex-col gap-4" autoComplete="off">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4" autoComplete="off">
         <span className="relative">
           <input
-            type="email"
-            name=""
-            id=""
+            type="name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             className="bg-neutral-700 text-white py-2 rounded-md w-full pl-10 pr-5 outline-none focus:border-yellow-500 "
             autoComplete="off"
           />
@@ -25,13 +60,15 @@ export default function AdminLogin() {
         <span className="relative">
           <input
             type="password"
-            name=""
-            id=""
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className="bg-neutral-700 text-white py-2 rounded-md w-full pl-10 pr-5 outline-none focus:border-yellow-500 "
             autoComplete="off"
           />
           <FaKey className="absolute left-3 top-1/2 transform -translate-y-1/2 text-white" />
         </span>
+
+        {error && <p className="text-red-500">{error}</p>}
 
         <button
           type="submit"
