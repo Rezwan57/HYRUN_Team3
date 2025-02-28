3b8437ae03bd4a04183c8f2d6b93a8076fed5e5b
Rezwan <rezwan57@outlook.com>
2025-02-28 10:49:38 +0000

    Updated the Product

--- src/app/(admin)/admin/products/page.jsx
+++ src/app/(admin)/admin/products/page.jsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation';
 export default function page() {
 
   const router = useRouter()
+  
 
   const handleAddProductClick = () => {
     router.push("/admin/products/add-product");
@@ -15,6 +16,7 @@ export default function page() {
     <>
       <div className="h-[120vh]">
         <button onClick={handleAddProductClick}>Add Product</button>
+
       </div>
     </>
   );
